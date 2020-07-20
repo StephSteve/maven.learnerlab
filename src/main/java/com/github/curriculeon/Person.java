a9182e4002f3b9fd72c3afcf8f21745e03b777a5
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-20 10:36:24 -0400

    Part one learner lab edits

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,24 @@
 package com.github.curriculeon;
 
 public class Person {
+    final Long id;
+    String name;
 
+    Person(long id, String name){
+        this.id = id;
+        this.name = "";
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
