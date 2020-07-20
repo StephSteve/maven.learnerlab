8b237697bf725f61cd1d1a995546173f05c4face
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-20 14:40:17 -0400

    added tests finished part one

---
+++ src/test/java/com/github/curriculeon/PersonTest.java
@@ -0,0 +1,15 @@
+package com.github.curriculeon;
+
+import junit.framework.TestCase;
+
+public class PersonTest extends TestCase {
+
+    public void testGetId() {
+    }
+
+    public void testTestGetName() {
+    }
+
+    public void testTestSetName() {
+    }
+}
