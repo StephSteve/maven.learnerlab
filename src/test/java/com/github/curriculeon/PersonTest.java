74f42a3dbde3f453ab0f308fb5de170a1b36cab5
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-20 15:36:27 -0400

    update

--- src/test/java/com/github/curriculeon/PersonTest.java
+++ src/test/java/com/github/curriculeon/PersonTest.java
@@ -1,15 +1,19 @@
 package com.github.curriculeon;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class PersonTest extends TestCase {
+    public class TestPerson {
+        private void testConstructor(long expectedId, String expectedName) {
+            Person person = new Person(expectedId, expectedName);
+            long actualId = person.getId();
+            String actualName = person.getName();
+            Assert.assertEquals(actualId, expectedId);
+            Assert.assertEquals(actualName, expectedName);
+        }
 
-    public void testGetId() {
     }
 
-    public void testTestGetName() {
-    }
-
-    public void testTestSetName() {
-    }
 }
