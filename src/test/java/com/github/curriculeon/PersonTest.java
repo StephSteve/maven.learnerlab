8af07b90a5b93cbde357616fe7e9b3362d38d233
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-07-20 15:44:25 -0400

    updates from Leon going over assignment

--- src/test/java/com/github/curriculeon/PersonTest.java
+++ src/test/java/com/github/curriculeon/PersonTest.java
@@ -7,9 +7,17 @@ import org.junit.Test;
 public class PersonTest extends TestCase {
     public class TestPerson {
         private void testConstructor(long expectedId, String expectedName) {
+            //given
+            Long ecpectedId = 0L;
+            expectedName = "Some name";
+
+
+            //when
             Person person = new Person(expectedId, expectedName);
             long actualId = person.getId();
             String actualName = person.getName();
+
+            //then
             Assert.assertEquals(actualId, expectedId);
             Assert.assertEquals(actualName, expectedName);
         }
